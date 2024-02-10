117df1752bc2dcdcd05744c996a3e1df4e9eb3a5
emo20142email.vccs.edu <emo2014@email.vccs.edu>
2024-02-10 16:46:08 -0500

    .

--- index.js
+++ index.js
@@ -87,40 +87,14 @@ class DOMManager {
         `<div id="${house._id}" class = "card">
           <div class="card-header">
           <h2>${house.name}</h2>
-          <button class= "btn btn-danger" onclick= "DOMManager.deleteHouse('${house.id}')">Delete</button></div>
-
-          <div class="card-body">
-            <div class = "card">
-              <div class ="row">
-                <div class="col-sm">
-                  <input type= "text" id=${house._id}-room-name" class="form-control" placeholder="Room Name">
-                </div>
-                <div class="col-sm">
-                <input type= "text" id=${house._id}-room-area" class="form-control" placeholder="Room Area">
-              </div>
-              </div>
-            <button id=${house._id}-new-room" onclick="DOMManager.addRoom('${house._id}')" class= "btn btn-primary form-control"> Add</button>
-         </div>
-        </div>
-        </div> <br>`
+          <button class = "btn btn-danger" onclick="DOMManager.deleteHouse('${house._id}')">Delete</button>
+          </div>
+          </div>
+          `
       );
-        for(let room of house.rooms) {
-          $(`#${house._id}`).find('.card-body').append(
-            `<p>
-              <span id="name-${room._id}"><strong>Name </strong> ${room.name}</span>
-              <span id="area-${room._id}"><strong>Area </strong> ${room.area}</span>
-              <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house._id})', ${room._id}')">Delete Room</button>`
-          );
-        }
-  
     }
   }
 }
 
-$('#create-new-house').click(() => {
-  DOMManager.createHouse($('#new-house-name').val());
-  $('new-house-name').val('');
-
-});
 
 DOMManager.getALLHouses();
